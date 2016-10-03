4411011464232f1943e5e98858e7aa96281167ef
Thomas Pettersen <thomasp@mesan.no>
2016-10-03 19:22:09 +0200

    Rounded images

--- app/src/main/java/no/mesan/view/people/PeopleItemView.java
+++ app/src/main/java/no/mesan/view/people/PeopleItemView.java
@@ -2,13 +2,16 @@ package no.mesan.view.people;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.makeramen.roundedimageview.RoundedTransformationBuilder;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,30 +29,45 @@ public class PeopleItemView extends LinearLayout {
     @Bind(R.id.imageViewPerson)
     ImageView imageViewPerson;
 
+    private int borderColor;
+
     public PeopleItemView(Context context) {
         super(context);
+        init(context);
     }
 
     public PeopleItemView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public PeopleItemView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
-
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public PeopleItemView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
     }
 
+    private void init(Context context) {
+        borderColor = context.getResources().getColor(R.color.colorPrimary);
+    }
 
     public void bindTo(Person person) {
         textViewPersonFullName.setText(person.getFullName());
         textViewPersonShortName.setText(getContext().getString(R.string.short_name, person.getShortName()));
         imageViewPerson.setImageDrawable(null);
-        Picasso.with(getContext()).load(person.getProfileImageUrl()).into(imageViewPerson);
+
+        Transformation transformation = new RoundedTransformationBuilder()
+                .borderColor(borderColor)
+                .borderWidthDp(2)
+                .oval(true)
+                .build();
+
+        Picasso.with(getContext()).load(person.getProfileImageUrl()).transform(transformation).into(imageViewPerson);
     }
 
     @Override
