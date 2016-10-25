995d237c4b483ce020e61b9ec90b4210c6a04894
Torkjel Skjetne <Torkjel.Skjetne@norgesgruppen.no>
2016-10-25 18:24:24 +0200

    Laget branch og som smått begynt på feed

---
+++ app/src/main/java/no/mesan/faghelg/view/social/SocialAdapter.java
@@ -0,0 +1,22 @@
+package no.mesan.faghelg.view.social;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class SocialAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
