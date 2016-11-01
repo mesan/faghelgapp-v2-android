ccb23f334579efed8ed16da538cbc16094719f5e
Kristin Fladeby <kristint@mesan.no>
2016-11-01 23:47:49 +0100

    Prevent empty message sending

--- app/src/main/java/no/mesan/faghelg/view/message/MessageFragment.java
+++ app/src/main/java/no/mesan/faghelg/view/message/MessageFragment.java
@@ -1,15 +1,21 @@
 package no.mesan.faghelg.view.message;
 
-import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
 import no.mesan.faghelg.injector.components.AppComponent;
 import no.mesan.faghelg.injector.components.DaggerMessageFragmentComponent;
 import no.mesan.faghelg.model.MessageOutput;
@@ -25,9 +31,32 @@ public class MessageFragment extends BaseFragment {
     @Bind(R.id.editContent)
     EditText editContent;
 
+    @Bind(R.id.btnSend)
+    Button sendButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        sendButton.setEnabled(false);
+
+        return view;
+    }
+
     @Override
     protected int getContentViewId() {
         return R.layout.fragment_message;
+    }
+
+    @OnTextChanged(R.id.editContent)
+    public void textChanged(CharSequence s) {
+
+        if(s.toString().trim().length()==0){
+            sendButton.setEnabled(false);
+        } else {
+            sendButton.setEnabled(true);
+        }
     }
 
     @OnClick(R.id.btnSend)
